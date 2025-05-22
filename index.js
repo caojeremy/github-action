49f75d73cd946a90b43359617acde27b3483d815
Jeremy Cao <jeremy.cao@sap.com>
2025-05-22 10:25:26 +0800

    quick commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // index.js
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-try {
-    const context = github.context;
-    const author = context.payload.sender.login;
-    core.info(`👋 Hello, ${author}! Thanks for your contribution.`);
-} catch (error) {
+async function run() {
+  try {
+    const name = core.getInput('name');
+    console.log(`Hello, ${name}!`);
+  } catch (error) {
     core.setFailed(error.message);
+  }
 }
+
+run();
